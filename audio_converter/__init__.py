f23bd2c549e2cb0aee01d4f03e3e0c79c79b59d8
diehm <Lukas.Diehm@hs-Augsburg.de>
2021-06-14 14:26:37 +0200

    fixed app

--- audio_converter/__init__.py
+++ audio_converter/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+# from flask_admin import Admin
 from flask_migrate import Migrate
+from flask_security import Security, SQLAlchemyUserDatastore
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-app.config.from_object('config.py')
+app.config.from_object('config')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
