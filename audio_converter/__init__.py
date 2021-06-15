7881afb87f68f98452f83bf983bcda01a1f481b8
diehm <Lukas.Diehm@hs-Augsburg.de>
2021-06-15 18:02:30 +0200

    added login and register

--- audio_converter/__init__.py
+++ audio_converter/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 # from flask_admin import Admin
 from flask_migrate import Migrate
-# from flask_security import Security, SQLAlchemyUserDatastore
+from flask_security import Security, SQLAlchemyUserDatastore
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -14,6 +14,8 @@ from audio_converter import models
 
 # TODO: implement database models and add to SQLAlchemy
 # TODO: implement admin_models and initialize admin feature
+user_datastore = SQLAlchemyUserDatastore(db, models.User, models.Role)
+security = Security(app, user_datastore)
 
 from audio_converter import admin_models
 
