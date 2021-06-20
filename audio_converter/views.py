5b7bf9b6c430eecb77a89ee4b4c4e5861884f9a8
Marcel Arndt <master.tww@web.de>
2021-06-20 21:58:22 +0200

    Add basic jinja utilization
    Signed-off-by: Marcel Arndt <master.tww@web.de>

--- audio_converter/views.py
+++ audio_converter/views.py
@@ -7,6 +7,17 @@ from audio_converter import app
 
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
+
+
+@app.route('/convert')
+def convert():
+    return render_template('convert.html')
+
+
+app.route('/signin')
+def login():
+    return render_template('signin.html')
 
