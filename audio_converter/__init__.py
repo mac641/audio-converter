a133e0de08ec2fef81c756cc2d068c90a7a5a363
diehm <Lukas.Diehm@hs-Augsburg.de>
2021-06-21 10:22:47 +0200

    added flask Mail

--- audio_converter/__init__.py
+++ audio_converter/__init__.py
@@ -3,6 +3,7 @@ from flask_admin import Admin
 from flask_migrate import Migrate
 from flask_security import Security, SQLAlchemyUserDatastore
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -21,5 +22,6 @@ admin = Admin(app, name='Audio Converter', template_mode='bootstrap3')
 from audio_converter import admin_models
 
 from audio_converter import views
+mail = Mail(app)
 
 # TODO: initialize security feature
