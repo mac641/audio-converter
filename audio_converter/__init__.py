fcac1080df7727cef86fdabb8a066f3e1e61d1d8
mac641 <master.tww@web.de>
2021-08-16 12:15:25 +0200

    Add flask-dropzone (does not run yet)
    Signed-off-by: mac641 <master.tww@web.de>

--- audio_converter/__init__.py
+++ audio_converter/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_admin import Admin
+from flask_dropzone import Dropzone
 from flask_migrate import Migrate
 from flask_security import Security, SQLAlchemyUserDatastore
 from flask_sqlalchemy import SQLAlchemy
@@ -13,8 +14,8 @@ migrate = Migrate(app, db)
 
 from audio_converter import models
 
-# TODO: implement database models and add to SQLAlchemy
-# TODO: implement admin_models and initialize admin feature
+# TODO: Guard admin access
+# TODO: Add admin features, e.g. admin can reset user passwords, etc.
 user_datastore = SQLAlchemyUserDatastore(db, models.User, models.Role)
 security = Security(app, user_datastore)
 
@@ -24,4 +25,4 @@ from audio_converter import admin_models
 from audio_converter import views
 mail = Mail(app)
 
-# TODO: initialize security feature
+dropzone = Dropzone(app)
--- audio_converter/views.py
+++ audio_converter/views.py
@@ -5,35 +5,36 @@ from flask_security import login_required
 from werkzeug.utils import secure_filename
 from audio_converter import app
 
+# TODO: decorate with multiple routes -> '/', '/index', '/logout' have to redirect to '/'
 
 @app.route('/')
-@app.route('/home')
 def home():
     return render_template('home.html')
 
 @app.route('/login')
 def login():
-    return render_template('login_user.html')
-
+    return render_template('security/login_user.html')
 
 @app.route('/register')
 def register():
-    return render_template('register_user.html')
+    return render_template('security/register_user.html')
 
 @app.route('/logout')
 def logout():
     return render_template('home.html')
 
-
-@app.route('/convert')
+@app.route('/convert', methods=['POST', 'GET'])
 def convert():
-    return render_template('convert.html')
+    if request.method == 'POST':
+        # Do something...
+        f = request.files.get('file')
+        f.save(os.path.join(app.config['UPLOAD_PATH'], f.filename))
 
+    return render_template('convert.html')
 
 @app.route('/imprint')
 def imprint():
     return render_template('imprint.html')
-
 
 @app.route('/privacy')
 def privacy():
