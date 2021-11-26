95277a7838a389a135c803f7baa1604803f33593
Lukas Diehm <Lukas.Diehm@hs-augsburg.de>
2021-11-26 11:26:10 +0100

    Issue#2 Add default admin user to database

--- create_db.py
+++ create_db.py
@@ -1,3 +1,18 @@
-from audio_converter import models
+from audio_converter import models, user_datastore
+import config
+import datetime
 
 models.db.create_all()
+
+adminRole = user_datastore.create_role(name='admin',
+                                       description='Administrator')
+adminUser = user_datastore.create_user(email=config.MAIL_USERNAME,
+                                       password=config.ADMIN_PASSWORD,
+                                       active=True,
+                                       confirmed_at=datetime.datetime.now())
+user_datastore.add_role_to_user(user=adminUser,
+                                role=adminRole)
+user_datastore.create_role(name='user',
+                           description='normal user')
+
+models.db.session.commit()
