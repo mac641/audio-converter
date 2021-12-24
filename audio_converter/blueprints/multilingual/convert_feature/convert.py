48b44bfaf85caa429a4302b3d2af9a8672244bf4
mac641 <marcel.arndt1@icloud.com>
2021-12-24 15:14:47 +0100

    Fix issue where uploaded files would not be stored
    in anonymous folder, if no user was logged in
    
      - Add some doc comments to utils.py
      - Minor refactorings

--- audio_converter/blueprints/multilingual/convert_feature/convert.py
+++ audio_converter/blueprints/multilingual/convert_feature/convert.py
@@ -20,8 +20,8 @@ allowed_audio_file_types = app.config['ALLOWED_AUDIO_FILE_TYPES']
 
 def process(request):
     utils.create_path(conversion_path)
-    specific_conversion_path = utils.set_path(conversion_path)
-    app.logger.info(specific_conversion_path)
+    specific_conversion_path = utils.set_db_path(conversion_path)
+    app.logger.debug(specific_conversion_path)
 
     if not current_user.is_authenticated:
         app.logger.info('Clean up old converted files...')
--- audio_converter/blueprints/multilingual/utils.py
+++ audio_converter/blueprints/multilingual/utils.py
@@ -1,14 +1,12 @@
 import os
 import shutil
-from datetime import datetime
 
 from flask import g
 from flask_login import current_user
 
+from audio_converter import app
 from audio_converter import db
 from audio_converter.models import User
-
-from audio_converter import app
 
 
 def delete_path(path):
@@ -21,36 +19,58 @@ def create_path(path):
         os.mkdir(path)
 
 
+def set_db_path(path):
+    """
+    Generate path based on user specific data. (Signed-In / Not Signed-In)
+    Commit it to the specific user entry in the database
+    Otherwise, return a folder called 'anonymous'.
+    :param path: string
+    :return: string
+    """
+    if current_user.is_authenticated:
+        g.user = current_user.get_id()
+        user = User.query.filter_by(fs_uniquifier=g.user).first()
+        user.convert += 1
+        db.session.commit()
+        return os.path.join(path, g.user, str(user.convert))
+    else:
+        return os.path.join(path, 'anonymous')
+
+
+def get_db_path(path):
+    """
+    Fetch a user specific path from the database.
+    :param path: string
+    :return: string
+    """
+    if current_user.is_authenticated:
+        g.user = current_user.get_id()
+        user = User.query.filter_by(fs_uniquifier=g.user).first()
+        return os.path.join(path, g.user, str(user.convert))
+    else:
+        return os.path.join(path, 'anonymous')
+
+
 def get_uploaded_files(upload_path):
+    """
+    Get all files from the upload path and return them as list of strings.
+    :param upload_path: string
+    :return: <string>[]
+    """
     files: list[str] = [f for f in os.listdir(upload_path) if os.path.isfile(os.path.join(upload_path, f))]
     return files
 
 
 def move_files(source_path, files, dest_path):
+    """
+    Move files from source_path to dest_path.
+    :param source_path: string
+    :param files: <string>[]
+    :param dest_path: string
+    """
     for file in files:
         # Check if file already exists in destination
         path = os.path.join(dest_path, file)
         if not os.path.exists(path):
             result = shutil.move(os.path.join(source_path, file), path)
             app.logger.info('Moved ' + result)
-
-
-def set_path(path):
-    if current_user.is_authenticated:
-        g.user = current_user.get_id()
-        create_path(os.path.join(path, g.user))
-        user = User.query.filter_by(fs_uniquifier=g.user).first()
-        user.convert += 1
-        db.session.commit()
-        return os.path.join(path, g.user, str(user.convert))
-    else:
-        return os.path.join(path, "anonymous")
-
-
-def get_path(path):
-    if current_user.is_authenticated:
-        g.user = current_user.get_id()
-        user = User.query.filter_by(fs_uniquifier=g.user).first()
-        return os.path.join(path, g.user, str(user.convert))
-    else:
-        return os.path.join(path, "anonymous")
