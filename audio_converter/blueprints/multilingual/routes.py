288327012668038e27a47205c4104fc16af0ffcf
mac641 <marcel.arndt1@icloud.com>
2021-12-29 14:50:45 +0100

    Add feature where zipping files for download
    is also possible by passing a list of paths
    
      - Refactor and reduced code duplication

--- audio_converter/blueprints/multilingual/routes.py
+++ audio_converter/blueprints/multilingual/routes.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import redirect, render_template, request, Blueprint, g, abort, after_this_request, url_for, send_file
+from flask import redirect, render_template, request, Blueprint, g, abort, after_this_request, url_for
 from flask_babelex import gettext
 from flask_login import current_user
 from flask_security import RegisterForm, ConfirmRegisterForm, views, unauth_csrf, auth_required
@@ -17,7 +17,7 @@ from werkzeug.local import LocalProxy
 
 from audio_converter import app
 from audio_converter.blueprints.multilingual.convert_feature.convert import process
-from audio_converter.blueprints.multilingual.convert_feature.download import zip_converted_files
+from audio_converter.blueprints.multilingual.convert_feature.download import zip_converted_files, zip_list, send_archive
 from audio_converter.blueprints.multilingual.convert_feature.upload import upload
 from audio_converter.models import Track
 
@@ -514,46 +514,7 @@ def convert_done():
 def convert_download():
     zip_archive = zip_converted_files()
     app.logger.info('Received zip compression status: ' + ', '.join(map(str, zip_archive)))
-    if zip_archive[2] == 200:
-        try:
-            app.logger.info('Send ' + zip_archive[0])
-            # TODO: Get download_name dynamically
-            return send_file(zip_archive[0], as_attachment=True, download_name='converted.zip',
-                             attachment_filename='converted.zip')
-        except FileNotFoundError:
-            app.logger.error('File not found!')
-            return abort(404)
-    else:
-        return abort(zip_archive[2])
-
-
-@multilingual.route('/download_history', methods=['GET', 'POST'])
-def download_history():
-    if not current_user.is_authenticated:
-        return redirect(url_for('multilingual.login'))
-    else:
-        if request.method == 'POST':
-            tracks_form = request.form.getlist('download-tracks')
-            links = []
-            for i in tracks_form:
-                track = Track.query.filter_by(id=i).first()
-                links.insert(-1, os.path.join(track.path, track.trackname + track.format))
-
-                # TODO: add here functionality for download tracks with a list of paths.
-
-        zip_archive = zip_converted_files()
-        app.logger.info('Received zip compression status: ' + ', '.join(map(str, zip_archive)))
-        if zip_archive[2] == 200:
-            try:
-                app.logger.info('Send ' + zip_archive[0])
-                # TODO: Get download_name dynamically
-                return send_file(zip_archive[0], as_attachment=True, download_name='converted.zip',
-                                 attachment_filename='converted.zip')
-            except FileNotFoundError:
-                app.logger.error('File not found!')
-                return abort(404)
-        else:
-            return abort(zip_archive[2])
+    return send_archive(zip_archive)
 
 
 @multilingual.route('/settings')
@@ -581,6 +542,23 @@ def history():
 
         return render_template('multilingual/history.html', title='Audio-Converter - ' + gettext('History'),
                                lang=g.lang_code, tracks=tracks)
+
+
+@multilingual.route('/history_download', methods=['GET', 'POST'])
+def history_download():
+    if not current_user.is_authenticated:
+        return redirect(url_for('multilingual.login'))
+
+    if request.method == 'POST':
+        tracks_form = request.form.getlist('download-tracks')
+        links = []
+        for i in tracks_form:
+            track = Track.query.filter_by(id=i).first()
+            links.insert(-1, os.path.join(track.path, track.trackname + track.format))
+
+        zip_archive = zip_list(links)
+        app.logger.info('Received status for zipping selected files: ' + ', '.join(map(str, zip_archive)))
+        return send_archive(zip_archive)
 
 
 @multilingual.route('/imprint')
